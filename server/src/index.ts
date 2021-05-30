6b19d82f45c0a1618f217dbca26b54972c0d197f
yun8boo <yuma051521@gmail.com>
2021-05-30 23:57:14 +0900

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,44 @@
-import express from 'express';
+import { ApolloServer, IResolvers } from 'apollo-server'
 
-const app = express();
-const port = process.env.PORT || 8000
+let links = [{
+  id: 'link-0',
+  description: 'Fullstack tutorial for GraphQL',
+  url: 'www.howtographql.com',
+}]
 
-app.listen(port, () => {
-  console.log(`listening on port: ${port}`);
+const typeDefs = `
+  type Query {
+    info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`
+
+const resolvers: IResolvers<any, any> | IResolvers<any, any>[]
+= {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 })
+
+server
+  .listen()
+  .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+  );
